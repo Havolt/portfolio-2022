50c18c60bd64858abc689b9bfa292371a7b2db4c
markf <markfitz815@gmail.com>
2022-03-29 20:42:26 +0100

    Add Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  const [showHeader, setShowHeader] = useState(true);
+
+  return <div className="App">{showHeader && <Header></Header>}</div>;
 }
 
 export default App;
