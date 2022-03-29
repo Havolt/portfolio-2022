366b5fe4f4b60061e06828844fc8f4f2564bc0d4
markf <markfitz815@gmail.com>
2022-03-29 20:31:58 +0100

    setup serving of files from build folder

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
+// Import Modules
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = 3000;
 
+// Serve static files
+const root = require("path").join(__dirname, "../frontend", "build");
+app.use(express.static(root));
+
+// Load Main Page
 app.get("/", (req, res) => {
-  res.send("Sending text");
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 
+// Set port for site to run on
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port ${port}`);
 });
