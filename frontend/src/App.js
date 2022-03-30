2c8464309147b92c1ccf3f6309b0d58ece334993
markf <markfitz815@gmail.com>
2022-03-30 21:51:57 +0100

    Add varables css file and add bg color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "./App.css";
+import "./styles/App.css";
 
 import Header from "./components/Header";
 
 function App() {
-  const [showHeader, setShowHeader] = useState(true);
+  const [showHeader, setShowHeader] = useState(false);
 
   return <div className="App">{showHeader && <Header></Header>}</div>;
 }
