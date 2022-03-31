71abbae0100fd3ccf042fc57cf249754f67c67bf
markf <markfitz815@gmail.com>
2022-03-31 21:28:45 +0100

    Add skeleton for footer section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 import "./styles/App.css";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [showHeader, setShowHeader] = useState(false);
+  const [showFooter, setShowFooter] = useState(false);
 
-  return <div className="App">{showHeader && <Header></Header>}</div>;
+  return (
+    <div className="App">
+      {showHeader && <Header></Header>}
+
+      {showFooter && <Footer></Footer>}
+    </div>
+  );
 }
 
 export default App;
