7c9a587eb61cf9449c15e4f6bc99b5840dd8d1b2
markf <markfitz815@gmail.com>
2022-04-05 18:09:04 +0100

    Add nav to center of screen

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -4,7 +4,7 @@ import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <button className={`${styles.nav__link}`}>About</button>
       <button className={`${styles.nav__link}`}>Companies</button>
       <button className={`${styles.nav__link}`}>Contact</button>
