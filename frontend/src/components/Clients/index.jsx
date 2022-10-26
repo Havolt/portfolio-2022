3e261b900f028f74881fb6c84f58cbc609431600
markf <markfitz815@gmail.com>
2022-10-26 19:01:11 +0100

    Add styles file to clients section

--- frontend/src/components/Clients/index.jsx
+++ frontend/src/components/Clients/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+
+import styles from "./Clients.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Clients() {
   return (
     <div>
-      <FontAwesomeIcon icon="fa-solid fa-angle-down" />
+      <FontAwesomeIcon
+        className={styles.clients__expand}
+        icon="fa-solid fa-angle-down"
+      />
     </div>
   );
 }
