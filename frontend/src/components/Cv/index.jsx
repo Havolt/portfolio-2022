5d44345d52696dc54449538d752bb42814efc006
markf <markfitz815@gmail.com>
2022-11-05 21:02:23 +0000

    Add CV link

--- frontend/src/components/Cv/index.jsx
+++ frontend/src/components/Cv/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Cv.module.css";
+import cvPdf from "./mark_fitzpatrick_cv.pdf";
 
 function Cv() {
   return (
     <div className={styles.cv}>
-      <button className={styles.cv__button}>
+      <a
+        href={cvPdf}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.cv__button}
+      >
         <FontAwesomeIcon icon="fa-solid fa-file-arrow-down" />
-        <span className={styles.cv__button__description}>Download CV</span>
-      </button>
+        <span className={styles.cv__button__description}>CV</span>
+      </a>
     </div>
   );
 }
