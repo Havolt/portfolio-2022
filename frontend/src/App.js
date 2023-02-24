08c99249703e694757a215ae263c2f5ca19db935
markf <markfitz815@gmail.com>
2023-02-24 19:36:46 +0000

    Stop importing unused components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,24 @@ import "./styles/App.css";
 
 import { SectionProvider } from "./context";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 import Bio from "./components/Bio";
-import Clients from "./components/Clients";
-import Contact from "./components/Contact";
+// import Clients from "./components/Clients";
+// import Contact from "./components/Contact";
 import Cv from "./components/Cv";
 import Nav from "./components/Nav";
 
 function App() {
-  const [showHeader, setShowHeader] = useState(false);
-  const [showFooter, setShowFooter] = useState(false);
-  const [showContact, setShowContact] = useState(false);
+  // const [showHeader, setShowHeader] = useState(false);
+  // const [showFooter, setShowFooter] = useState(false);
+  // const [showContact, setShowContact] = useState(false);
   const [showCv, setShowCv] = useState(true);
 
   return (
     <div className="App">
       <SectionProvider>
-        {showHeader && <Header></Header>}
+        {/* {showHeader && <Header></Header>} */}
         <Bio></Bio>
 
         <Nav></Nav>
@@ -28,8 +28,8 @@ function App() {
         {/* <Clients></Clients> */}
 
         {showCv && <Cv></Cv>}
-        {showContact && <Contact></Contact>}
-        {showFooter && <Footer></Footer>}
+        {/* {showContact && <Contact></Contact>} */}
+        {/* {showFooter && <Footer></Footer>} */}
       </SectionProvider>
     </div>
   );
