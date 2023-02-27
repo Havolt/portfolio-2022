bf5efa355362ab27a5e7923a14d3acd0afce8cf7
markf <markfitz815@gmail.com>
2023-02-27 19:13:39 +0000

    Clean up app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,20 @@
-import { useState } from "react";
 import "./styles/App.css";
 
 import { SectionProvider } from "./context";
 
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Bio from "./components/Bio";
 // import Clients from "./components/Clients";
-// import Contact from "./components/Contact";
 import Cv from "./components/Cv";
 import Nav from "./components/Nav";
 
 function App() {
-  // const [showHeader, setShowHeader] = useState(false);
-  // const [showFooter, setShowFooter] = useState(false);
-  // const [showContact, setShowContact] = useState(false);
-  const [showCv, setShowCv] = useState(true);
-
   return (
     <div className="App">
       <SectionProvider>
-        {/* {showHeader && <Header></Header>} */}
         <Bio></Bio>
-
         <Nav></Nav>
-
         {/* <Clients></Clients> */}
-
-        {showCv && <Cv></Cv>}
-        {/* {showContact && <Contact></Contact>} */}
-        {/* {showFooter && <Footer></Footer>} */}
+        <Cv></Cv>
       </SectionProvider>
     </div>
   );
